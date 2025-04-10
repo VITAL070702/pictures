2821b1c7f82450aafbe69f6a15033e042bcc09f2
VITAL070702 <yvitalraquel@gmail.com>
2025-04-10 14:44:40 -0700

    Cambie api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from " axios";
 
 const searchImages = async(term)=>{
-    const url='https://api.unsplash.com/search/photos/?client_id=4TqZ2_Ck_2WuZmZMeZJD6KcXds9yrj_6q47mIYc_HR0'
+    const url='https://api.unsplash.com/search/photos/?client_id=q09fgnZ8-aVfK49kjrj6PTarVnXKIx83-Y7c3d8k0Lk'
     const response =await axios.get(url, {
         params:{
             query:'cars'
